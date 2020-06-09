907a2bcb6e9d6b8d674dfaeb7571febea845a222
AtilioA <atiliodadalto@hotmail.com>
2020-06-09 18:40:04 -0300

    Tweak cron schedule

--- bot_config.js
+++ bot_config.js
@@ -18,7 +18,7 @@ cron.schedule('0,15,30,45 * * * *', async () => {
 });
 
 // Keep INMETBot awake
-cron.schedule('0,30 0,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', () => {
+cron.schedule('0,20,40 0,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', () => {
   axios.get('https://inmetbot.herokuapp.com/');
 });
 
