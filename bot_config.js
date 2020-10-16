7d973d00a40b3042f5426610af2c9286afc9f858
AtilioA <atiliodadalto@hotmail.com>
2020-10-16 12:41:40 -0300

    Remove timezone from cron schedule

--- bot_config.js
+++ bot_config.js
@@ -19,7 +19,7 @@ cron.schedule('0,15,30,45 * * * *', async () => {
 });
 
 // Keep INMETBot awake
-cron.schedule('0,40 7-21,23 * * *', { timezone : "America/Sao_Paulo" }, () => {
+cron.schedule('0,40 10-23,2 * * *', () => {
   axios.get('https://inmetbot.herokuapp.com/');
 });
 
